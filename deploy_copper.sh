798e7602af5e270dc582e7c6577aa79e268b955d
leoedin <leorampen@gmail.com>
2024-03-28 13:13:29 +0000

    Change hackathon version, add deploy script

---
+++ deploy_copper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rsync --exclude target -r ./ hack@192.168.50.33:/home/hack/
+ssh hack@192.168.50.33
+# http://192.168.50.33:3000/camera
+# ssh-copy-id hack@192.168.50.33
+
+# on the pi - cargo run --release
